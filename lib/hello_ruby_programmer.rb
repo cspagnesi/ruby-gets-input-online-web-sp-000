5702bbf31a9dabc2362bd11f32bd1ef9bd9cb2d6
Christopher Spagnesi <christopherspagnesi@gmail.com>
2020-11-11 12:08:58 +0000

    Automatically backed up by Learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,6 +1,7 @@
-def greeting(name)
-  puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
+def greeting (name)
+  puts "Hello, #{name}.  It's nice to meet you."
 end
 
-
-
+def feelings (adjective)
+  puts "So you are doing #{adjective} today."
+end
